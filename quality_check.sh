dc3a550c73c241674f4fe65ab11c42dacf00593b
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-17 14:34:16 +0100

    Added multiprocessing to fastqc quality checking script

--- quality_check.sh
+++ quality_check.sh
@@ -1,8 +1,12 @@
 loop_dir=/localdisk/data/BPSM/AY21/fastq/
+jobs=5
 mkdir fastqc_output
+(
 for file in $loop_dir*; do
+        ((i=i%jobs)); ((i++==0)) && wait
 	if [[ "$file" == *".fq.gz" ]]; then
 		echo -e "$file"
-		fastqc $file -o fastqc_output --quiet
+		fastqc $file -o fastqc_output --quiet &
 	fi
 done
+)
