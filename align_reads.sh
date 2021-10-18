478cf769eccb5319fdaff70d334f01e31fe598a9
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-18 15:33:10 +0100

    Fixed error with verbose output of progress bar

--- align_reads.sh
+++ align_reads.sh
@@ -11,15 +11,13 @@ while read ID Sample Replicate Time Treatment End1 End2; do
 		:
 	else
 		count=$((count+1))
-		echo -e "$ID"
 		bowtie2 --quiet \
    		--threads 5 \
 		-x tcongo_index \
 		-1 /localdisk/data/BPSM/AY21/fastq/$End1 \
 		-2 /localdisk/data/BPSM/AY21/fastq/$End2 \
 		-S $(pwd)/bowtie_alignments/$ID$Sample$Replicate$Time.sam &
-		echo -e -n "\rAligned $count of $info_file_lines sample \
-                paired-end reads..." 
+		echo -e -n "\rAligned $count of $info_file_lines sample paired-end reads..." 
 	fi
 done < "$info_file"
 echo -e "\nFinished alignment of all sample reads to reference genome!"	
