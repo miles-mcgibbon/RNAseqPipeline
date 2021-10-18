1365f25c6765668f269595409076f0c033875f63
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-18 15:48:46 +0100

    Minor changes to echo statements

--- create_bowtie_database.sh
+++ create_bowtie_database.sh
@@ -2,4 +2,4 @@
 echo -e "Creating bowtie2-indexed reference genome..."
 ref_genome=/localdisk/data/BPSM/AY21/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz
 bowtie2-build "$ref_genome" tcongo_index --threads 5 --quiet
-echo -e "Reference genome indexed!"
+echo -e "Reference genome indexed..."
