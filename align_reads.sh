6362625d98eeb40e04d1af5f2ae9f6fea39dd500
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-18 15:23:23 +0100

    Temporarily removed multiprocessing and added progress reporting while script runs

--- align_reads.sh
+++ align_reads.sh
@@ -1,17 +1,23 @@
 unset IFS
 IFS=$'\t'
 mkdir bowtie_alignments
+unset count
 info_file=/localdisk/data/BPSM/AY21/fastq/100k.fqfiles
+info_file_lines=$(cat $info_file | grep 100k | wc -l)
 while read ID Sample Replicate Time Treatment End1 End2; do
 	if [[ "$Sample" == "Sample" ]]; then
 		:
 	else
+		count=$((count+1))
 		echo -e "$ID"
 		bowtie2 --quiet \
    		--threads 5 \
 		-x tcongo_index \
 		-1 /localdisk/data/BPSM/AY21/fastq/$End1 \
 		-2 /localdisk/data/BPSM/AY21/fastq/$End2 \
-		-S $(pwd)/bowtie_alignments/$ID$Sample$Replicate$Time.sam 
+		-S $(pwd)/bowtie_alignments/$ID$Sample$Replicate$Time.sam
+		echo -e -n "\rAligned $count of $info_file_lines sample \
+                paired-end reads..." 
 	fi
-done < "$info_file"	
+done < "$info_file"
+echo -e "\nFinished alignment of all sample reads to reference genome!"	
