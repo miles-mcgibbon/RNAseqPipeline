cfbe8b8fdb5462413ba8d4410b867f09ef406c00
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-18 15:27:04 +0100

    Reduced verbosity of bowtie2 index file creation and re-introduced multiprocessing to bowtie2 alignment script

--- align_reads.sh
+++ align_reads.sh
@@ -1,10 +1,12 @@
 unset IFS
 IFS=$'\t'
 mkdir bowtie_alignments
+jobs=5
 unset count
 info_file=/localdisk/data/BPSM/AY21/fastq/100k.fqfiles
 info_file_lines=$(cat $info_file | grep 100k | wc -l)
 while read ID Sample Replicate Time Treatment End1 End2; do
+	((i=i%jobs)); ((i++==0)) && wait
 	if [[ "$Sample" == "Sample" ]]; then
 		:
 	else
@@ -15,7 +17,7 @@ while read ID Sample Replicate Time Treatment End1 End2; do
 		-x tcongo_index \
 		-1 /localdisk/data/BPSM/AY21/fastq/$End1 \
 		-2 /localdisk/data/BPSM/AY21/fastq/$End2 \
-		-S $(pwd)/bowtie_alignments/$ID$Sample$Replicate$Time.sam
+		-S $(pwd)/bowtie_alignments/$ID$Sample$Replicate$Time.sam &
 		echo -e -n "\rAligned $count of $info_file_lines sample \
                 paired-end reads..." 
 	fi
--- create_bowtie_database.sh
+++ create_bowtie_database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e "Creating bowtie2-indexed reference genome..."
 ref_genome=/localdisk/data/BPSM/AY21/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz
-bowtie2-build "$ref_genome" tcongo_index --threads 5
+bowtie2-build "$ref_genome" tcongo_index --threads 5 --quiet
 echo -e "Reference genome indexed!"
