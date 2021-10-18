21ae6190dac73775128d60c3e793459d2a7e1eae
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-18 15:20:13 +0100

    Added progress report to fastqc processing

--- quality_check.sh
+++ quality_check.sh
@@ -1,12 +1,17 @@
 loop_dir=/localdisk/data/BPSM/AY21/fastq/
 jobs=5
+unset count
 mkdir fastqc_output
+all_files=$(ls /localdisk/data/BPSM/AY21/fastq/*fq.gz| wc -l)
+echo -e "$all_files"
 (
 for file in $loop_dir*; do
         ((i=i%jobs)); ((i++==0)) && wait
 	if [[ "$file" == *".fq.gz" ]]; then
-		echo -e "$file"
 		fastqc $file -o fastqc_output --quiet &
+		count=$((count+1))
+		echo -e -n "\rProcessed $count of $all_files reads with fastqc..."
 	fi
 done
 )
+echo -e "\nFinished quality check of paired-end reads!"
