c22b33be6f850cfb3ad5cd4736ceade888c2a54d
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-19 12:44:10 +0100

    Additional functionality added to convert_to_bam.sh - samtools sort now performed - samtools index now performed - bedtools multicov now performed and output to txt file in gene_counts/

--- convert_to_bam.sh
+++ convert_to_bam.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 bowtie_sams="$(pwd)/bowtie_alignments/"
-output_dir="$(pwd)/bam_files/"
 mkdir bam_files
+mkdir gene_counts
 wd=$(pwd)
 for file in $bowtie_sams*; do
         filename="${file//.sam/.bam}"
-        filename="${filename//bowtie_alignments/bam_files}"
+        bam_filename="${filename//bowtie_alignments/bam_files}"
+	sort_filename="${bam_filename//.bam/.sorted.bam}"
+	count_filename="${bam_filename//.bam/.txt}"
+	count_filename="${count_filename//bam_files/gene_counts}"
 	echo -e "$filename"
-	samtools view -S -b $file > $filename
+	samtools view -S -b $file > $bam_filename
+	samtools sort $bam_filename > $sort_filename
+	samtools index $sort_filename
+        bedtools multicov \
+	-bams $sort_filename \
+	-bed /localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed \
+	> $count_filename
 done
