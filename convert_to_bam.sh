f0fcbf928fc650854ae542b477e1b23c31d6cb51
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-22 09:21:24 +0100

    Changed bedtools multicov command - now takes a list of all bam files instead of being called once for each file

--- convert_to_bam.sh
+++ convert_to_bam.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 bowtie_sams="$(pwd)/bowtie_alignments/"
+bowtie
 mkdir bam_files
 mkdir gene_counts
 wd=$(pwd)
@@ -10,11 +11,12 @@ for file in $bowtie_sams*; do
 	count_filename="${bam_filename//.bam/.txt}"
 	count_filename="${count_filename//bam_files/gene_counts}"
 	echo -e "$filename"
-	samtools view -S -b $file > $bam_filename
+	samtools view -S -b $file > $bam_filename 
 	samtools sort $bam_filename > $sort_filename
-	samtools index $sort_filename
-        bedtools multicov \
-	-bams $sort_filename \
-	-bed /localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed \
-	> $count_filename
+	samtools index $sort_filename 
 done
+
+bedtools multicov \
+        -bams $sort_filename \
+        -bed /localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed \
+        > $count_filename
