2e2be73b1978b6fff630b8217df23af4bcc2e18d
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-22 13:38:37 +0100

    Added awk code to iteratively calculate fold change for each sample data column - code to add headers currently not working

--- parse_counts.sh
+++ parse_counts.sh
@@ -1,12 +1,61 @@
+
+# calculate the mean for all samples
+awk '{sum = 0; 
+     for (i = 6; i <= NF; i++) sum += $i; 
+     sum /= NF; print sum}' sample_counts/all_sample_counts.tsv > sample_means.tsv
+
+# add the means as a column to the sample counts
+paste sample_counts/all_sample_counts.tsv sample_means.tsv > fold_data.tsv 
+
+# count the number of sample columns in the gene counts data
+mean_col=$(awk '{ FS = "\t" } ; {print NF}' fold_data.tsv | tail -1)
+declare -i start_data_index=6
+end_data_index=$(($mean_col - 1))
+
+num_cols="$mean_col"
+# loop through sample columns and calculate fold change for each sample
+for i in $(seq $start_data_index $end_data_index); do
+        num_cols=$((num_cols+1))	
+        echo -e "$num_cols"
+	echo -e "$mean_col"
+	echo -e "$i"
+	awk -v data_col=$i -v fold_col=$num_cols -v mean_c=$mean_col 'BEGIN {FS="\t";OFS = "\t"};
+	{
+	if($data_col == 0||$mean_c == 0)
+	{
+	$fold_col = 0
+	}
+	else
+	{
+	$fold_col = ($data_col/$mean_c)
+	}
+	print $0}' fold_data.tsv > temp_fold_data.tsv && mv temp_fold_data.tsv fold_data.tsv
+done
+
 declare -a headers=("Count" "Count2" "TCIL" "Protein")
-echo -e -n "TCA" > counts_with_headers.tsv
+echo -n "TCA" > fold_data_with_headers.tsv
 for header in "${headers[@]}"; do
-	echo -e	-n "\t$header" >> counts_with_headers.tsv
+        echo -e -n "\t$header" >> fold_data_with_headers.tsv
 done
 bowtie_sams="$(pwd)/bowtie_alignments/"
+sample_files=()
+
 for file in $bowtie_sams*; do
-	filename="$(basename $file)"
-	echo -e	"$filename"
-	echo -e -n "\t$filename" >> counts_with_headers.tsv
-done	 
-cat counts.txt >> counts_with_headers.tsv
+    sample_files+=("$file")
+done
+for file in "${sample_files[@]}"; do
+        filename="$(basename $file)"
+	echo -e "$filename"
+        echo -e -n "\t$filename" >> fold_data_with_headers.tsv
+done
+echo -e "\tMean" >> fold_data_with_headers.tsv
+for file in "${sample_files[@]}"; do
+        filename="$(basename $file)"
+        suffix="$_fold_change_from_mean"
+	echo -e "$filename$suffix"
+        echo -e -n "\t$filename$suffix" >> fold_data_with_headers.tsv
+done
+echo -e "\n" >> fold_data_with_headers.tsv
+read -n 1 -p "Input Selection:" "mainmenuinput"
+cat fold_data.tsv >> fold_data_with_headers.tsv 
+
