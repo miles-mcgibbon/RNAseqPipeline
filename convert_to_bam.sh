3f8ff812d6a990ccaa2f798a139d77510a1f6894
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-22 15:35:29 +0100

    Changed bedtools usage so that each bam file is processed separately

--- convert_to_bam.sh
+++ convert_to_bam.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 bowtie_sams="$(pwd)/bowtie_alignments/"
-bowtie
 mkdir bam_files
 mkdir gene_counts
 wd=$(pwd)
@@ -8,15 +7,14 @@ for file in $bowtie_sams*; do
         filename="${file//.sam/.bam}"
         bam_filename="${filename//bowtie_alignments/bam_files}"
 	sort_filename="${bam_filename//.bam/.sorted.bam}"
-	count_filename="${bam_filename//.bam/.txt}"
+	count_filename="${bam_filename//.bam/.tsv}"
 	count_filename="${count_filename//bam_files/gene_counts}"
 	echo -e "$filename"
 	samtools view -S -b $file > $bam_filename 
 	samtools sort $bam_filename > $sort_filename
 	samtools index $sort_filename 
-done
-sorted_bams="$(pwd)/bam_files/"
-bedtools multicov \
-        -bams $sorted_bams*.sorted.bam \
+	bedtools multicov \
+        -bams $sort_filename \
         -bed /localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed \
-        > all_sample_counts.tsv
+        > $count_filename
+done
