567da3389816698c7b3e59bb648a75de9a71af52
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-22 19:40:31 +0100

    Group means script now organises gene count data into subfolders by group

--- group_means.sh
+++ group_means.sh
@@ -9,14 +9,32 @@ info_file=/localdisk/data/BPSM/AY21/fastq/100k.fqfiles
 groups=$(tail -n +2 $info_file | cut -f2 | sort -u )
 time_points=$(tail -n +2 $info_file | cut -f4 | sort -u )
 treatments=$(tail -n +2 $info_file | cut -f5 | sort -u )
-echo -e "$time_points"
-echo -e "$treatments"
+mkdir group_files
 IFS=$'\n'
 for group in $groups; do
 	for time_point in $time_points; do
 		for treatment in $treatments; do
-			echo -e "$group $time_point $treatment"
-		done
+			mkdir group_files/${group}_${time_point}_${treatment}
+			ls -d gene_counts/* | grep gene_counts/${group}_._${time_point}_${treatment} | xargs cp -t group_files/${group}_${time_point}_${treatment}/ 2>/dev/null || : 
+			rmdir group_files/${group}_${time_point}_${treatment} 2>/dev/null || :
+		done	
 	done
 done
 
+for folder in $(pwd)/group_files/*; do
+	group=$(basename $folder)
+	echo -e "$group"
+	for file in $folder/*; do
+		filename=$(basename $file)
+		filename="${filename//.tsv/_summary.tsv}"
+		IFS=$'\t'
+		if [[ "$file" == *"_1_"* ]];then
+			cat $file | cut -f4,5,6 > "$folder/${filename}"
+		else
+			cat $file | cut -f6 > "$folder/${filename}"
+			
+		fi
+	ls -d group_files/$group/* | grep summary | xargs paste >> "${group}_final.tsv" 
+	done	
+	
+done
