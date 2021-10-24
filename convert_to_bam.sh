7ad70563d4e352fadab1ff8065368e36801abf49
B111679-2021 <milesmcgibbon@gmail.com>
2021-10-24 22:54:02 +0100

    Added progress indicator to bam conversion/gene_count script

--- convert_to_bam.sh
+++ convert_to_bam.sh
@@ -3,13 +3,17 @@ bowtie_sams="$(pwd)/bowtie_alignments/"
 mkdir bam_files
 mkdir gene_counts
 wd=$(pwd)
+unset count
+total_files=$(ls bowtie_alignments/*sam|wc -l)
+echo "Converting reads and calculating counts data..."
 for file in $bowtie_sams*; do
+	count=$((count+1))
         filename="${file//.sam/.bam}"
         bam_filename="${filename//bowtie_alignments/bam_files}"
 	sort_filename="${bam_filename//.bam/.sorted.bam}"
 	count_filename="${bam_filename//.bam/.tsv}"
 	count_filename="${count_filename//bam_files/gene_counts}"
-	echo -e "$filename"
+	echo -e -n "\rProcessing file $count of $total_files: $(basename $file)"
 	samtools view -S -b $file > $bam_filename 
 	samtools sort $bam_filename > $sort_filename
 	samtools index $sort_filename 
